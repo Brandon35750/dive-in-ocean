b90893b5ae3c896297387bea0e4af66e513bf388
briac louazel <briac@laposte.net>
2020-11-24 12:10:50 +0100

    routes defined

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :tags, only: [:show, :index, :new, :create]
+  resources :species, only: [:show]
+  resources :users, only: [:show, :edit, :update]
+  resource  :gallery, only: [:show]
 end
