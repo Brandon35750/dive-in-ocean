f1140417007fc9880683e0a79513301ff2b4e0f0
briac louazel <briac@laposte.net>
2020-11-24 18:32:38 +0100

    create new route for index of a user tag

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :tags, only: [:show, :index, :new, :create]
+  resources :tags, only: [:show, :index]
   resources :species, only: [:show]
   resources :users, only: [:show, :edit, :update]
   resource  :gallery, only: [:show]
+  namespace :user do
+    resources :tags, only: [:index, :new, :create]
+  end
 end
