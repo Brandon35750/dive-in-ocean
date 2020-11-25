17d8f440b3a240b9b1a35c5701eb3617951b050a
briac louazel <briac@laposte.net>
2020-11-25 13:15:43 +0100

    add show to tags_controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,8 @@
 class TagsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def show
+    @tag = Tag.find(params[:id])
+    @user = @tag.user
+  end
 end
