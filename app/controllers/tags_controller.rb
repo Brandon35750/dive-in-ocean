4f9aa2f2b00981f846a6219e93aea7a469d3dfcc
Marek Chrzestek <march@gmx.fr>
2020-11-25 12:52:38 +0100

    controller tag en cours

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,13 @@
 class TagsController < ApplicationController
+    def index
+        if params[:query].present?
+          @tags = Tag.where(species: params[:query])
+        else
+          @tags = Tag.all
+        end
+    end
+
+    def show
+    end
 end
+
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,14 @@ class Tag < ApplicationRecord
   belongs_to :user
   belongs_to :species
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [],
+    associated_against: {
+      species: [ :name, :latin_name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
