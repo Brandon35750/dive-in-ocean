2c71e6a1fee52af86b2708b3d634338d9a4da92d
Marek Chrzestek <march@gmx.fr>
2020-11-30 16:28:41 +0100

    member can add a tag, not finish 100%

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,9 @@
 class TagsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
-
   skip_before_action :authenticate_user!, only: [:index, :show]
+
+
+  
 
   def show
     @tag = Tag.find(params[:id])
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,38 @@
 // init_mapbox.js
 import mapboxgl from 'mapbox-gl';
 
+
+const initMapboxNewTag = () => {
+  const mapElement = document.getElementById('map-new-tag');
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map-new-tag',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 0
+    });
+
+    const markers = [];
+    map.on('click', function (e) {
+      // document.getElementById('info').innerHTML =
+      // '<br />' +
+      // e.lngLat is the longitude, latitude geographical position of the event
+      const lngLat = e.lngLat.wrap();
+
+      const oldMarker = markers.pop();
+      if (oldMarker) oldMarker.remove();
+    
+      const newMarker = new mapboxgl.Marker()
+      .setLngLat([ lngLat.lng, lngLat.lat ])
+      .addTo(map);
+      markers.push(newMarker)
+      console.log(markers)
+    });   
+  };
+};
+
+
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -10,7 +42,6 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11'
     });
-
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
@@ -30,7 +61,7 @@ const initMapbox = () => {
     fitMapToMarkers(map, markers);
     }
 };
-export { initMapbox };
+export { initMapbox, initMapboxNewTag};
 
 // zoom effect de JJ
 // var map = new mapboxgl.Map({
